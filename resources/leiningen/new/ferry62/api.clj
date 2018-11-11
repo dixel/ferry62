34de9c199905b62f396f7b80345e44121180ae83
Vasiliy Avdiushkin <dixelt@gmail.com>
2018-11-11 17:31:31 +0100

    fix swagger1st app definition

--- resources/leiningen/new/ferry62/api.clj
+++ resources/leiningen/new/ferry62/api.clj
@@ -34,11 +34,11 @@
 
 {{#swagger1st}}
 (def app
-  (s1st/context :yaml-cp "{{ name }}-api.yaml")
-  (s1st/discoverer)
-  (s1st/mapper)
-  (s1st/parser)
-  (s1st/executor))
+  (-> (s1st/context :yaml-cp "{{ name }}-api.yaml")
+      (s1st/discoverer)
+      (s1st/mapper)
+      (s1st/parser)
+      (s1st/executor)))
 {{/swagger1st}}
 
 (mount/defstate api
